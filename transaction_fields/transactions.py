fd23835a9a55460c5010f484e9009c9c9a3779d7
SJKenny45 <sarajanedeveloper@gmail.com>
2023-04-07 00:35:10 +0100

    Update transactions.py

---
+++ transaction_fields/transactions.py
@@ -0,0 +1,28 @@
+from pyteal import *
+
+"""
+Simple RSVP App
+Methods:
+- pay: check if the account opted in, paid 1 ALGO, and create local states
+- check_paid: check if the account has paid
+"""
+
+
+
+if __name__ == "__main__":
+    import os
+    import json
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    approval, clear, contract = router.compile_program(version=8)
+
+    # Dump out the contract as json that can be read in by any of the SDKs
+    with open(os.path.join(path, "artifacts/contract.json"), "w") as f:
+        f.write(json.dumps(contract.dictify(), indent=2))
+
+    # Write out the approval and clear programs
+    with open(os.path.join(path, "artifacts/approval.teal"), "w") as f:
+        f.write(approval)
+
+    with open(os.path.join(path, "artifacts/clear.teal"), "w") as f:
+        f.write(clear)
