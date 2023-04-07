bdc984fe001a344107980e040e28d52b7d85f802
SJKenny45 <sarajanedeveloper@gmail.com>
2023-04-07 02:13:35 +0100

    Simple RSVP App

--- transaction_fields/transactions.py
+++ transaction_fields/transactions.py
@@ -7,7 +7,39 @@ Methods:
 - check_paid: check if the account has paid
 """
 
+router = Router(
+    "Simple RSVP App",
+    BareCallActions(
+        no_op = OnCompleteAction.create_only(Approve()),
+        opt_in = OnCompleteAction.call_only(Approve()),
+        close_out = OnCompleteAction.always(Approve()),
+    ),
+    clear_state = Approve(),
+ )
 
+@router.method
+def pay(pay: abi.PaymentTransaction):
+    return Seq(
+        # Checking if account is opted in
+        Assert(App.optedIn(Txn.sender(), Global.current_application_id())),
+        # Checking if payment txn has sent 1 algo
+        Assert(pay.get().amount() == Int(1000000)),
+        # Getting the receiver and checking if its current app address
+        Assert(pay.get().receiver() == Global.current_application_address()),
+        # Creating local state
+        App.localPut(Txn.sender(), Bytes("paid"), Bytes("True")),
+        Approve()
+    ) 
+
+@router.method
+def check_paid(*, output: abi.String):
+    # Reading the local state of the sender of txn
+    paid_state = App.localGet(Txn.sender(), Bytes("paid"))
+    return Seq(
+        # If its true out put the paid state
+        Assert(paid_state == Bytes("True")),
+        output.set(paid_state)
+    )
 
 if __name__ == "__main__":
     import os
